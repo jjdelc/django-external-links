ddf46b1c09b2530d2df4e61d7edc691d93bc09eb
Jj <jjdelc@gmail.com>
2009-07-23 19:42:46 -0500

    Adding admin

---
+++ external_links/admin/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin
+
+from external_links.models import LinkClick
+
+class LinkClickAdmin(admin.ModelAdmin):
+    search_fields = ('link', 'referer', 'ip_addr' )
+    date_hierarchy = 'date'
+    list_filter = ('site', )
+    list_display = ('link', 'referer', 'site', 
+        'ip_addr', 'date', 'time')
