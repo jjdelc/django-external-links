f8ced8cab051a279613148973783d262a085d1d3
jj <jj@sloggi.(none)>
2009-07-25 03:38:22 -0500

    Fixing permissions and template

--- external_links/views.py
+++ external_links/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django.http import HttpResponseRedirect
+from django.contrib.admin.views.decorators import staff_member_required
 
 from external_links.models import LinkClick
 
+@staff_member_required
 def external_link(request):
     """
     Redirects links and keeps track of them
