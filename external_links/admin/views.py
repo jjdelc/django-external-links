019fa4c9f5b0c69ac9558a7a038d02888dff20a5
jj <jj@sloggi.(none)>
2009-08-24 00:09:55 -0500

    Mistankenly decorated a view

--- external_links/admin/views.py
+++ external_links/admin/views.py
@@ -3,9 +3,11 @@
 from django.db.models import Count
 from django.template import RequestContext
 from django.views.generic.list_detail import object_list
+from django.contrib.admin.views.decorators import staff_member_required
 
 from external_links.models import LinkClick
 
+@staff_member_required
 def top_links(request):
     summary = \
     LinkClick.objects.values('link').annotate(Count('link')).order_by('-link__count')
