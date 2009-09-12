37effd99c97705b3ecd90d8c579dca449b248a4c
jj <jj@sloggi.(none)>
2009-09-12 13:43:31 -0500

    Now top clicked links works with the ModelAdmin.get_urls() method, demoved unused admn.views and admin.urls

--- external_links/admin/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import url, patterns
-
-urlpatterns = patterns('external_links.admin.views',
-    url(r'^top/$',
-        'top_links', name='external_link_top_links'),
-)
