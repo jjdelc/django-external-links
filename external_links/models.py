0fef80f78c5223a74134864ce3e699a26b6436c0
Jj <jjdelc@gmail.com>
2009-12-16 21:04:20 -0500

    Adding blocked ip list

--- external_links/models.py
+++ external_links/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
+from django.core.cache import cache
+
+BLOCKED_IPS_LIST = 'external-link:blocked-ips'
 
 class LinkClick(models.Model):
     """
@@ -24,6 +27,12 @@ class LinkClick(models.Model):
         """
         Update params based on Request object
         """
+        ip_addr = request.META['REMOTE_ADDR']
+
+        if ip_addr in BlockedIp.objects.get_ips():
+            # If it's a blocked IP, dont do anything 
+            return None
+
         user = None
         if request.user.is_authenticated():
             user = request.user
@@ -31,5 +40,39 @@ class LinkClick(models.Model):
         self.user = user
         self.site = Site.objects.get_current()
         self.referer = request.META.get('HTTP_REFERER','')
-        self.ip_addr = request.META['REMOTE_ADDR']
+        self.ip_addr = ip_addr
         self.save()
+
+class BlockedManager(models.Manager):
+    
+    def get_ips(self):
+        """
+        Returns a cached list of ip addresses
+        """
+        result = cache.get(BLOCKED_IPS_LIST, None)
+
+        if not result:
+            result = self.values_list('ip_addr', flat=True)
+            cache.set(BLOCKED_IPS_LIST, result, 60*60*24) # 1 day?
+
+        return result
+
+class BlockedIp(models.Model):
+    """
+    Contains a list of IPs that shouldn't be tracked, such as search engines
+    crawlers, etc.
+    """
+    ip_addr = models.IPAddressField()
+    name = models.CharField(max_length=128, blank=True)
+
+    objects = BlockedManager()
+
+    def __unicode__(self):
+        return self.ip_addr
+
+    def save(self, *args, **kwargs):
+        """
+        Clear the cached list of blocked IPs
+        """
+        cache.delete(BLOCKED_IPS_LIST)
+        super(BlockedIp, self).save(*args, **kwargs)
